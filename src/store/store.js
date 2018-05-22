260e2bec4bdec7fd358dd7f045e4fd8b58d0b30b
George Brata <bratageorge@gmail.com>
2018-05-22 15:55:07 +0300

    Display the data in component

--- src/store/store.js
+++ src/store/store.js
@@ -6,10 +6,25 @@ import VueAxios from 'vue-axios'
 Vue.use(Vuex)
 Vue.use(VueAxios, axios)
 
+let url = 'http://localhost:4000';
+
 export default new Vuex.Store({
   state: {
     coins: []
   },
-  actions: {},
-  mutations: {}
+  actions: {
+    loadCoins ({ commit }) {
+      axios
+        .get(`${url}/results`)
+        .then(r => r.data)
+        .then(coins => {
+          commit('SET_COINS', coins)
+        })
+    }
+  },
+  mutations: {
+    SET_COINS (state, coins) {
+      state.coins = coins
+    }
+  }
 })
