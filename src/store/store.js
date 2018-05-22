f1eb12ea0a6364dfb7c610d88a6095211dfa7eb6
George Brata <bratageorge@gmail.com>
2018-05-22 15:43:53 +0300

    Added mocked json db and vuex store

---
+++ src/store/store.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(Vuex)
+Vue.use(VueAxios, axios)
+
+export default new Vuex.Store({
+  state: {
+    coins: []
+  },
+  actions: {},
+  mutations: {}
+})
